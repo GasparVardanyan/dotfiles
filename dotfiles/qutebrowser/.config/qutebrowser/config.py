dd1a9019d844381fba3982f71007c1e1a180df30
GasparVardanyan <gaspar.vardanyan.mailbox@gmail.com>
2021-05-31 15:05:14 +0400

    update

--- dotfiles/qutebrowser/.config/qutebrowser/config.py
+++ dotfiles/qutebrowser/.config/qutebrowser/config.py
@@ -3,4 +3,9 @@ import dracula.draw
 # Load existing settings made via :set
 config.load_autoconfig()
 
-dracula.draw.blood(c)
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
