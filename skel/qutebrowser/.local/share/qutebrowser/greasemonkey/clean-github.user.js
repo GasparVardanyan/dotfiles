9dd60d33eedd4359248b790b231eee47b57f5d07
GasparVardanyan <gaspar_pm@proton.me>
2022-04-23 21:04:08 +0400

    update

---
+++ skel/qutebrowser/.local/share/qutebrowser/greasemonkey/clean-github.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name          Clean Github
+// @namespace     http://userstyles.org
+// @description	  clear bs from github homepage
+// @include       https://github.com/
+// @run-at        document-start
+// ==/UserScript==
+(function() {var css = [
+	".my-3 { display: none; }",
+].join("\n");
+if (typeof GM_addStyle != "undefined") {
+	GM_addStyle(css);
+} else if (typeof PRO_addStyle != "undefined") {
+	PRO_addStyle(css);
+} else if (typeof addStyle != "undefined") {
+	addStyle(css);
+} else {
+	var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
+}
+})();
