37271980c9ebb8df80fc8113a4e0dd2335b97171
GasparVardanyan <gaspar_pm@proton.me>
2024-09-20 16:57:06 +0400

    update

---
+++ skel/qutebrowser/.local/share/qutebrowser/greasemonkey/google-boolshit.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         google boolshit fixer
+// @version      1.0
+// @description  google boolshit fixer
+// @match        *://mail.google.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {setTimeout(function () {
+// Function to apply pointer cursor to all yellow-background elements
+	alert ("LOADED");
+    function removeAds() {
+        const ad = document.querySelectorAll('*');
+        ad.forEach(element => {
+			if (element.innerHTML == "Missing message notifications?") {
+				btns = element.parentElement.getElementsByTagName ("button");
+				for (i = 0; i < btns.length; i++)
+					if ("Continue" == btns [i].innerHTML) {
+						button = btns [i];
+
+						// Simulate a mouse down, up, and click event sequence
+						button.dispatchEvent(new MouseEvent('mousedown', { bubbles: true, cancelable: true }));
+						button.dispatchEvent(new MouseEvent('mouseup', { bubbles: true, cancelable: true }));
+						button.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true }));
+						element.parentNode.removeChild(element);
+						alert("FIXED");
+					}
+			}
+        });
+	}
+
+	window.addEventListener('load', removeAds);
+	removeAds ();
+
+	const observer = new MutationObserver(mutationsList => {
+		for(let mutation of mutationsList) {
+			if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+				removeAds();
+			} else if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+				removeAds();
+			}
+		}
+	});
+
+	observer.observe(document.documentElement, { childList: true, subtree: true, attributes: true });
+
+}, 5000);})();
