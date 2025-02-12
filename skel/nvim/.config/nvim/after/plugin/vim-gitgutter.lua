6a39e56b95e6dc809decd268560bbc7b11b9836a
GasparVardanyan <gaspar_pm@proton.me>
2025-02-12 08:29:25 +0400

    update

--- skel/nvim/.config/nvim/after/plugin/vim-gitgutter.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd [[
-	nmap <Leader>gd <Plug>(GitGutterPreviewHunk)
-	" nmap <Leader>gf <Plug>(GitGutterFold) " why doesn't work?
-	nmap <Leader>gf :GitGutterFold<cr>
-	nmap <Leader>gu <Plug>(GitGutterUndoHunk)
-	nmap ]h <Plug>(GitGutterNextHunk)
-	nmap [h <Plug>(GitGutterPrevHunk)
-]]
--- skel/nvim/.config/nvim/after/plugin/vim-tmux-navigator.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd [[
-	let g:tmux_navigator_no_mappings = 1
-
-	nnoremap <silent> <M-h> :<C-U>TmuxNavigateLeft<cr>
-	nnoremap <silent> <M-j> :<C-U>TmuxNavigateDown<cr>
-	nnoremap <silent> <M-k> :<C-U>TmuxNavigateUp<cr>
-	nnoremap <silent> <M-l> :<C-U>TmuxNavigateRight<cr>
-]]
